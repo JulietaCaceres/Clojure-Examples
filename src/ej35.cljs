814f6393be4673f6ea695d9f692d27e07bc43724
JulietaCaceres <julieta.agustina.caceres@gmail.com>
2022-11-07 20:12:56 -0300

    Adding exercise 35

--- src/ej35.cljs
+++ src/ej35.cljs
@@ -1,2 +1,20 @@
-hello world in clojure script (cljs)
+;a)
+; Definir las funciones filas-max-V y mas-V-o-F que, aplicadas a una matriz de V y F
+;(una lista de listas con los valores V y F), devuelvan, respectivamente:
+;a) El/los número/s de la/s fila/s en la/s que la cantidad de V es máxima, por ejemplo:
+;(filas-max-V '((V F V V F)(V V F V V)(F F F V F)(V V V F V))) → (1 3)
+
+(defn get-frecuencies [i l]
+      (conj [i] (get (frequencies l) 'V)))
+
+(defn get-max [M]
+      (reduce max (map last (map-indexed get-frecuencies M))) )
+
+
+(defn filas-max-V [M]
+      (map first (filter (fn [x] (= (last x) (get-max M)))  (map-indexed get-frecuencies M) )
+           )
+      )
+
+(print (filas-max-V '((V F V V F)(V V F V V)(F F F V F)(V V V F V))))
 
